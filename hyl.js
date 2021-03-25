96e4df85bdcf6c1bd819dc637c706e14789c87d4
Nell3582 <qianye1203@outlook.com>
2021-03-25 17:14:07 +0800

    update

--- hyl.js
+++ hyl.js
@@ -1,87 +1,3 @@
-// var body = $response.body;
-// var url = $request.url;
-// var obj = JSON.parse(body);
-
-// const path = "SettleHotWaterOrder";
-// const path1 = "EmpById";
-
-// function FormatDate () {
-//   var now = new Date();
-//   var year = now.getFullYear();
-//   var month = now.getMonth();
-//   var date = now.getDate();
-//   month = month + 1;
-//   if (month < 10){
-//       month = "0" + month;
-// }
-//   if (date < 10) {
-//    date = "0" + date;
-//   }
-//   var time = year + "-" + month + "-" + date;
-//   return time;
-// }
-// var today_time =  FormatDate();
-// if(today_time >= '2021-03-26'){
-//   notify()
-// } else {
-//   Unlock()
-// };
-
-// function Unlock() {
-//   if (url.indexOf(path1) != -1) {
-//     obj["data"]["balance"] = "100.00";
-//     body = JSON.stringify(obj);
-//     $done({ body });
-//   }
-
-//   if (url.indexOf(path) != -1) {
-//     let data = $request.body;
-//     data["amout"] = "0.18";
-//     data["surplus"] = "9.82";
-//     ResponseData = JSON.stringify(data);
-//     $done({ ResponseData });
-//   }
-
-//   if (url.indexOf(path) != -1) {
-//     obj["data"]["prestore"] = "10";
-//     obj["data"]["withhold"] = "0.09";
-//     obj["data"]["water_consumption"] = "2.9";
-//     obj["data"]["amount"] = "0.18";
-//     obj["data"]["surplus"] = "9.82";
-//     body = JSON.stringify(obj);
-//     $done({ body });
-//   }
-// };
-
-// function notify() {
-//   if (url.indexOf(path1) != -1) {
-//     obj["data"]["depts"] = "您的解锁文件已过有效期请联系>QQ>3058649832>获取新解锁文件";
-//     body = JSON.stringify(obj);
-//     $done({ body });
-//   }
-// }
-
-// var body = $response.body;
-// var url = $request.url;
-// var obj = JSON.parse(body);
-
-// const path = "SettleHotWaterOrder";
-// const path1 = "EmpById";
-
-// if (url.indexOf(path) != -1) {
-//   obj["data"]["prestore"] = "10";
-//   obj["data"]["withhold"] = "0.09";
-//   obj["data"]["water_consumption"] = "0.4";
-//   obj["data"]["amount"] = "0.09";
-//   obj["data"]["surplus"] = "9.91";
-//   body = JSON.stringify(obj);
-// }
-// if (url.indexOf(path1) != -1) {
-//   obj["data"]["mobile_phone"] = "17361014786";
-//   obj["data"]["balance"] = "100.00";
-//   body = JSON.stringify(obj);
-// }
-// $done({ body });
 
 var body = $response.body;
 var url = $request.url;
@@ -164,7 +80,7 @@ function Unlock() {
 }
 
 function notify() {
-  if (headers["Cookie"] !== key) {
+  if (tel !== key) {
     if (url.indexOf(path1) != -1) {
       obj["data"]["depts"] = "用户鉴权失败,请联系>QQ>3058649832>获取解锁文件";
     } else if (today_time >= finaltime) {
